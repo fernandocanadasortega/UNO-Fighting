733c10e9d6a038995463479cc135d90da54b0e79
Azriel <thenapo212@gmail.com>
2024-06-08 03:58:25 +0200

    Modificación página de perfil, añadidas estadisticas destacadas

--- src/app/material-components.module.ts
+++ src/app/material-components.module.ts
@@ -9,6 +9,8 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   exports: [
@@ -21,7 +23,9 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
     MatTabsModule,
     MatDatepickerModule,
     MatNativeDateModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatCardModule,
+    MatTooltipModule
   ],
 })
 export class MaterialComponentsModule {

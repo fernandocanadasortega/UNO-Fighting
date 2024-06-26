4ebea8b5d27e93d7106b3ac82b88739db6d847b5
Azriel <thenapo212@gmail.com>
2024-06-27 01:41:26 +0200

    Creación animación de despliegue de search-bar

--- src/app/pages/tournament/game-session/new-session-game/new-session-game.page.ts
+++ src/app/pages/tournament/game-session/new-session-game/new-session-game.page.ts
@@ -14,10 +14,24 @@ import { MaterialComponentsModule } from '../../../../material-components.module
 })
 export class NewSessionGamePage implements OnInit {
 
+  public searchOverlay: boolean = false;
+
   constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
     console.log('Carga la información de la carta');
+  }
+
+  refreshGameSession(refreshEvent: any) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      refreshEvent.target.complete();
+    }, 1000);
+  }
+
+  showSearchOverlay() {
+    // Poner un panel que tenga una animación y que se deslice por encima y cubra todo el header (El panel tiene el searchbar y un boton de lupa con el fondo de color)
+    this.searchOverlay = this.searchOverlay ? false : true;
   }
 
   public closeModal() {
